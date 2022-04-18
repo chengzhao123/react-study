75bcdb8605cc3830fbd21948cd5249ab1a0540dd
chengzhao123 <1029063696@qq.com>
2022-04-18 21:00:40 +0800

    dasd

--- src/component/04_处理事件/04_处理事件.jsx
+++ src/component/04_处理事件/04_处理事件.jsx
@@ -1,22 +1,47 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button, Divider, Tag } from 'antd'
+
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            status: true
+            status: true,
+            list: [
+                {name: '测试人员1'},
+                {name: '测试人员2'},
+                {name: '测试人员3'},
+                {name: '测试人员4'},
+                {name: '测试人员5'},
+                {name: '测试人员6'},
+                {name: '测试人员7'},
+                {name: '测试人员8'},
+            ]
         }
     }
-    handleClick =() => {
+    handleClick = () => {
         this.setState(e => ({
             status: !e.status
+        }))
+    }
+    handleNav = (e) => {
+        e.preventDefault() //阻止系统的默认事件 阻止跳转到www.baidu.com界面
+        this.setState(r => ({
+            status: !r.status
         }))
     }
     render() {
         return (
             <div>
-                {this.state.status ? 'ON' : 'OFF'}
+                <h2>{this.state.status ? 'ON' : 'OFF'}</h2>
                 <Button type='primary' size='mini' onClick={this.handleClick}>添加</Button>
+                <a href='www.baidu.com' onClick={this.handleNav}>组件跳转事件</a>
+                <Divider orientation="left">用户列表</Divider>
+                <div>
+                    {this.state.list.forEach((item) => {
+                        <Tag>{item.name}</Tag>
+                    })
+                    }
+                </div>
             </div>
         )
     }
